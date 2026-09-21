070c762ef53ca39aa4c3a69f3a43fe387e7a2850
Kilovoid <soma.savin@gmail.com>
2026-09-21 16:50:36 +0300

    refactor: add method to check if grnti is valid, naming conventions fix

---
+++ SQLWerk/Services/GrntiService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SQLWerk.Services
+{
+    public class GrntiService
+    {
+        public string GrntiReader(string? codestring)
+        { 
+            if (codestring != null)
+            {
+                codestring = codestring.Trim();
+                string[] codes = codestring.Split(new[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < codes.Length; i++)
+                {
+                    codes[i] = codes[i].Trim();
+                    if (!(Regex.IsMatch(codes[i], @"^\d{2}\.\d{2}\.\d{2}$") || Regex.IsMatch(codes[i], @"^\d{2}\.\d{2}$") || Regex.IsMatch(codes[i], @"^\d{2}$")))
+                    {
+                        Console.WriteLine($"Неверный формат");
+                        return codestring;
+                    }
+                }
+
+                string coderesult = string.Join(", ", codes);
+                return coderesult;
+            }
+            return codestring;
+        }
+    }
+}
---
+++ SQLWerk/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SQLWerk.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+}
